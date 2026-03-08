dec1442f7f7dc8886b25dff83186a9828fbf6ecc
koushingnu <koushin1022apple@gmail.com>
2026-03-08 19:28:51 +0900

    基盤完成:こーしん

---
+++ app/hiyoko-meister/page.tsx
@@ -0,0 +1,22 @@
+export default function HiyokoMeisterPage() {
+  return (
+    <main className="min-h-screen bg-white p-8">
+      <div className="max-w-4xl mx-auto">
+        <a href="/" className="text-blue-600 hover:underline mb-4 inline-block">
+          ← トップに戻る
+        </a>
+        
+        <h1 className="text-5xl font-black mb-4">ひよこ鑑定士</h1>
+        <p className="text-xl text-gray-600 mb-8">
+          ひよこの鑑定士
+        </p>
+
+        <div className="border-2 border-black p-8">
+          <p className="text-lg">
+            ここにひよこ鑑定士のコンテンツを追加していきます。
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
